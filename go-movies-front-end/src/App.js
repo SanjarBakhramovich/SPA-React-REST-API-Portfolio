a37bd131a925404c66cc80dc9caac7713ccaa71b
Sanjar <sanjarbakhramovich@gmail.com>
2023-10-21 13:32:47 +0500

    set application layout

--- go-movies-front-end/src/App.js
+++ go-movies-front-end/src/App.js
@@ -1,8 +1,46 @@
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-      Go watch a movie!
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go watch a movie</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Log in</span>
+          </a>
+        </div>
+        <hr className="mb-3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">
+                Home
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Movies
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Genres
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                Add movies
+              </a>   <a href="#!" className="list-group-item list-group-item-action">
+                Manage catalogue
+              </a>
+              <a href="#!" className="list-group-item list-group-item-action">
+                GraphQL
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10"></div>
+      </div>
     </div>
   );
 }
