396df3f91b3f0abb5ae9646b1179440cd1a2a610
Sanjar <sanjarbakhramovich@gmail.com>
2023-10-24 15:39:00 +0500

    return Back end api, route and handlers add

---
+++ go-movies-back-end/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// set application config
+	var app application
+	// read from cmd line
+
+	// connect to the DB
+	app.Domain = "example.com"
+	// start a web server
+	log.Println("Starting application on port", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go-movies-back-end/go.mod
@@ -0,0 +1,8 @@
+module backend
+
+go 1.21.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.10
+)
